9270aa397192349cb9e4a5ce1c3bfeada113eaf7
João Vitor <jsoareez@gmail.com>
2020-10-04 13:15:47 -0300

    ui clone done

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background: #EBFFF5;
+  }
+    .colored:nth-child(2) {
+    background: #D6FFEB;
+  }
+    .colored:nth-child(3) {
+    background: #C2FFE1;
+  }
+    .colored:nth-child(4) {
+    background: #ADFFD8;
+  }
+    .colored:nth-child(5) {
+    background: #99FFCE;
+  }
+    .colored:nth-child(6) {
+    background: #85FFC4;
+  }
+    .colored:nth-child(7) {
+    background: #70FFBA;
+  }
+`;
+
+export const Spacer = styled.div`
+  height: 5vh;
+  background: #70FFBA;
+`;
